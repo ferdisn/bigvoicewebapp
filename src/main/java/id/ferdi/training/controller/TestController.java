538e36e83bc90b1b76d1e10f988ad281cd86901a
Ferdi_S672 <Ferdi_S672@MTPC734.local>
2018-04-18 18:28:09 +0800

    Correcting path for Tomcat TestController.java

--- src/main/java/id/ferdi/training/controller/TestController.java
+++ src/main/java/id/ferdi/training/controller/TestController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "TestController", urlPatterns = {"TestController"})
+@WebServlet(name = "TestController", urlPatterns = {"/TestController"})
 public class TestController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
